fdcfdce0ae79c8acbcc4200f8586a90370619463
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-12-06 13:07:28 +0000

    Initial Commit

---
+++ ScrollFix/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ScrollFix
+//
+//  Created by Freddie Farr on 06/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ScrollFixTests/ScrollFixTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollFixTests.swift
+//  ScrollFixTests
+//
+//  Created by Freddie Farr on 06/12/2025.
+//
+
+import Testing
+
+struct ScrollFixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
