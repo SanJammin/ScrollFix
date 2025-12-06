ce7a2491b2cdfebe19c39e09fb9b0b1f8cbe1852
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-12-06 14:58:18 +0000

    Move to MacOS only

---
+++ ScrollFixMac/ScrollFixMacTests/ScrollFixMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollFixMacTests.swift
+//  ScrollFixMacTests
+//
+//  Created by Freddie Farr on 06/12/2025.
+//
+
+import Testing
+@testable import ScrollFixMac
+
+struct ScrollFixMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
