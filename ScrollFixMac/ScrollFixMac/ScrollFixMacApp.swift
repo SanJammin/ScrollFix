140a3197994f90b5770b0cc7b2ea39fc60cdd1ce
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-12-08 20:03:20 +0000

    Add menu function

--- ScrollFixMac/ScrollFixMac/ScrollFixMacApp.swift
+++ ScrollFixMac/ScrollFixMac/ScrollFixMacApp.swift
@@ -3,12 +3,19 @@ import SwiftUI
 @main
 struct ScrollFixMacApp: App {
     private let scrollEventTap = ScrollEventTap()
+    private var statusBarController: StatusBarController?
 
     init() {
         scrollEventTap.start()
+
+        // Set up the status bar icon and quit action
+        statusBarController = StatusBarController {
+            NSApplication.shared.terminate(nil)
+        }
     }
 
     var body: some Scene {
+        // You can keep the window for now, or remove it later.
         WindowGroup {
             ContentView()
         }
--- ScrollFixMac/ScrollFixMac/StatusBarController.swift
+++ ScrollFixMac/ScrollFixMac/StatusBarController.swift
@@ -1,8 +1,38 @@
-//
-//  StatusBarController.swift
-//  ScrollFixMac
-//
-//  Created by Freddie Farr on 08/12/2025.
-//
+import Cocoa
 
-import Foundation
+final class StatusBarController {
+    private let statusItem: NSStatusItem
+
+    init(onQuit: @escaping () -> Void) {
+        // Create an item in the menu bar with variable length
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            button.title = "⇅"   // simple placeholder title; you can swap for an image later
+            button.toolTip = "ScrollFix"
+        }
+
+        // Build a basic menu
+        let menu = NSMenu()
+
+        let quitItem = NSMenuItem(
+            title: "Quit ScrollFix",
+            action: #selector(quitTapped),
+            keyEquivalent: "q"
+        )
+        quitItem.target = self
+
+        menu.addItem(quitItem)
+
+        statusItem.menu = menu
+
+        self.onQuit = onQuit
+    }
+
+    // Stored callback for quitting the app
+    private var onQuit: (() -> Void)?
+
+    @objc private func quitTapped() {
+        onQuit?()
+    }
+}
